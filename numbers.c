100a2d7454ac6bf7d27a9fc63f08717977faacd0
Joaquín Chacón <94719297+joaquincgp@users.noreply.github.com>
2022-10-21 14:52:18 -0500

    Update numbers.c

--- numbers.c
+++ numbers.c
@@ -1,49 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Determinar el numero mayor y el numero menor de 6 numeros ingresados por teclado*/
+/*Determinar el numero mayor y el menor entre 6 distintos
 
-int num1;
-int num2;
-int num3;
-int num4;
-int num5;
-int num6;
-
-int main(int argc, char *argv[]) {
-	printf("Ingrese el numero 1: ");
-	scanf("%i", &num1);
-	printf("Ingrese el numero 2: ");
-	scanf("%i", &num2);
-	printf("Ingrese el numero 3: ");
-	scanf("%i", &num3);
-	printf("Ingrese el numero 4: ");
-	scanf("%i", &num4);
-	printf("Ingrese el numero 5: ");
-	scanf("%i", &num5);
-	printf("Ingrese el numero 6: ");
-	scanf("%i", &num6);
-	
-	if((num1>num2) && (num1>num3) && (num1>num4) && (num1>num5) && (num1>num6)){
-		printf("El numero 1 (%d) es el mayor", num1);
-	}
-	
-	if((num2>num1) && (num2 > num3) && (num2>num4) && (num2>num5) && (num2>num6)){
-		printf("El numero 2 (%d) es el mayor", num2);
-	}
-	if((num3>num1) && (num3 > num2) && (num3>num4) && (num3>num5) && (num3>num6)){
-		printf("El numero 3 (%d) es el mayor", num3);
-	}
-	if((num4>num1) && (num4 > num2) && (num4>num3) && (num4>num5) && (num4>num6)){
-		printf("El numero 4 (%d) es el mayor", num4);
-	}
-	if((num5>num1) && (num5 > num2) && (num5>num3) && (num5>num4) && (num4>num6)){
-		printf("El numero 5 (%d) es el mayor", num5);
-	}
-	if((num6>num1) && (num6 > num2) && (num6>num3) && (num6>num5) && (num6>num4)){
-		printf("El numero 6 (%d) es el mayor", num6);
-	}
-	
-	
-	return 0;
-}
+#include <stdio.h> 
+#include <conio.h> 
+  
+ int n1; 
+ int n2; 
+ int n3; 
+ int n4; 
+ int n5; 
+ int n6; 
+  
+  
+ int main() 
+ { 
+     printf("Primer numero (solo enteros): \n"); 
+     scanf("%d", &n1); 
+     printf("Segundo numero (solo enteros): \n"); 
+     scanf("%d", &n2); 
+      printf("Tercer numero (solo enteros): \n"); 
+     scanf("%d", &n3); 
+     printf("Cuarto numero (solo enteros): \n"); 
+     scanf("%d", &n4); 
+      printf("Quinto numero (solo enteros): \n"); 
+     scanf("%d", &n5); 
+     printf("Sexto numero (solo enteros): \n"); 
+     scanf("%d", &n6); 
+      
+     if ((n1>=n2) && (n1>=n3) && (n1>=n4) && (n1>=n5) && (n1>=n6)){ 
+         printf("%d es el mayor  \n", n1); 
+     }else if ((n1<=n2) && (n2>=n3) && (n2>=n4) && (n2>=n5) && (n2>=n6)){ 
+         printf("%d es el mayor \n", n2); 
+     }else if ((n1<=n3) && (n2<=n3) && (n3>=n4) && (n3>=n5) && (n3>=n6)){ 
+         printf("%d es el mayor \n", n3); 
+     }else if ((n1<=n4) && (n2<=n4) && (n3<=n4) && (n4>=n5) && (n4>=n6)){ 
+         printf("%d es el mayor \n", n4); 
+     }else if ((n1<=n5) && (n2<=n5) && (n3<=n5) && (n4<=n5) && (n5>=n6)){ 
+         printf("%d es el mayor \n", n5); 
+     }else if ((n1<=n6) && (n2<=n6) && (n3<=n6) && (n4<=n6) && (n5<=n6)){ 
+         printf("%d es el mayor \n", n6); 
+     } 
+      
+      if ((n1<=n2) && (n1<=n3) && (n1<=n4) && (n1<=n5) && (n1<=n6)){ 
+         printf("%d es el menor \n", n1); 
+     }else if ((n1>=n2) && (n2<=n3) && (n2<=n4) && (n2<=n5) && (n2<=n6)){ 
+         printf("%d es el menor \n", n2); 
+     }else if ((n1>=n3) && (n2>=n3) && (n3<=n4) && (n3<=n5) && (n3<=n6)){ 
+         printf("%d es el menor \n", n3); 
+     }else if ((n1>=n4) && (n2>=n4) && (n3>=n4) && (n4<=n5) && (n4<=n6)){ 
+         printf("%d es el menor \n", n4); 
+     }else if ((n1>=n5) && (n2>=n5) && (n3>=n5) && (n4>=n5) && (n5<=n6)){ 
+         printf("%d es el menor \n", n5); 
+     }else if ((n1>=n6) && (n2>=n6) && (n3>=n6) && (n4>=n6) && (n5>=n6)){ 
+         printf("%d es el menor \n", n6); 
+     } 
+     
+     return 0; 
+ }
