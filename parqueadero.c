a86c174f140a4db495d24b05b0dcc5199e82f507
Joaquín Chacón <94719297+joaquincgp@users.noreply.github.com>
2022-11-11 10:09:58 -0500

    Update parqueadero.c

--- parqueadero.c
+++ parqueadero.c
@@ -1,31 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
 /*Calcular el precio de parqueadero ingresando la hora de ingreso y de salida. El valor por hora es 1,2. Si se tiene tarjeta de afiliado tiene descuento de 35%*/
 
-float hEntrada;
-float hSalida;
-float horasParqueo;
+int hEntrada;
+int hSalida;
+int mEntrada;
+int mSalida;
+int horasParqueo;
+int minutosParqueo;
 float tarifa;
+int hExtra;
+int horasAdicionales;
+int residuo;
 
 int main(int argc, char *argv[]) {
 	printf("Bienvenido al parqueadero: \n");
 	printf("Ingrese la hora de entrada: \n");
-	scanf("%f", &hEntrada);
+	scanf("%i:%i", &hEntrada, &mEntrada);
 	printf("Ingrese la hora de salida: \n");
-	scanf("%f", &hSalida);
+	scanf("%i:%i", &hSalida, &mSalida);
+	
 	if((hEntrada>=0)&&(hEntrada<=24)&&(hSalida>=0)&&(hSalida<=24)){
-		printf("Su hora de ingreso: %f horas \n", hEntrada);
-		printf("Su hora de salida: %f horas \n", hSalida);
+		printf("Su hora de ingreso: %i:%i horas \n", hEntrada, mEntrada);
+		printf("Su hora de salida: %i:%i horas \n", hSalida, mSalida);
 		horasParqueo = hSalida - hEntrada;
+		minutosParqueo = mSalida - mEntrada;
+		
+	    if(minutosParqueo<0){
+	        horasAdicionales = hSalida + 60;
+    	    horasParqueo = horasParqueo - 1;
+    	    hExtra = horasAdicionales - hEntrada;
+    	    printf("Ha estado parqueado %i:%i horas \n", horasParqueo,hExtra);
+    	        if(hExtra>60){
+    	        residuo = hExtra / 60;
+    	        horasParqueo = horasParqueo + residuo;
+	    }
+	}
+		
 		tarifa = ceil(horasParqueo) * 1.2;
 		printf("El valor total de parqueo es de %f dolares \n", tarifa);
-		printf("Ha estado parqueado %f horas \n", horasParqueo);
+		printf("Ha estado parqueado %i horas \n", horasParqueo);
 		
 	}
 	
 	return 0;
 }
-
-
