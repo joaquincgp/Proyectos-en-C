091fb65669448e191a858eccfbac728db79a3d67
Joaquín Chacón <94719297+joaquincgp@users.noreply.github.com>
2022-11-15 17:38:46 -0500

    Add files via upload

---
+++ comisionesXventa.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Implemente el juego 3 en raya. Considerar que se tendran 2 jugadores y que cada casillero esta denontado por una letra
+
+Calcular el sueldo final de un vendedor considerando su sueldo basico y las comisiones por venta. Las comsiiones son 
+en base al monto de cada venta considerando: A(100-200) comision es 10%, B entre (201-300) comision es 12% y C (>301) 15%. 
+Sueldo basico y numero de ventas ingreado por teclado*/
+
+//Variables
+float sueldo;
+	float ventas;
+	float total;
+	int caso;
+	int nuevaVenta;
+	float calcularComisiones(int caso);
+	float monto;
+	int contador;
+
+int main(int argc, char *argv[]) {
+	printf("Cual es el salario basico del trabajador: \n");
+	scanf("%f", &sueldo);
+	printf("Quiere ingresar una venta? 1 para SI y 2 para NO: \n");
+	scanf("%i", &nuevaVenta);
+
+	while(nuevaVenta==1){
+		printf("Ingrese las ganancias en ventas, para calcular las comisiones: \n" );
+		scanf("%f", &ventas);
+		contador = 0;
+		if((ventas>=100)&&(ventas<=200)){
+			caso = 1;
+			monto += calcularComisiones(1);	
+			contador++;
+		}else if((ventas>200)&&(ventas<=300)){
+			caso = 2;
+			monto += calcularComisiones(2);
+			contador++;
+		}else if(ventas>300){
+			caso = 3;
+			monto += calcularComisiones(3);
+			contador++;
+		}
+		printf("Quiere ingresar otra venta?: \n");
+		scanf("%i", &nuevaVenta);
+	}
+	
+	
+	total = monto + sueldo;
+	printf("El total que recibe el trabajador es de %f dolares, acumulando una cantidad de %f dolares en comisiones por haber tenido un total de %i ventas \n", total, monto,contador);
+	
+	return 0;
+}
+
+float calcularComisiones(int caso){
+	float comisiones;
+	float acumulado;
+		switch(caso){
+			case 1:
+				comisiones = 0.1;
+				acumulado = ventas + comisiones*ventas;
+				break;
+			case 2:
+				comisiones = 0.12;
+				acumulado = ventas + comisiones*ventas;
+				break;
+			case 3:
+				comisiones = 0.15;
+				acumulado = ventas + comisiones*ventas;
+				break;			
+		}
+		return acumulado;
+	}
